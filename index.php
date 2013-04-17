4865398e1f9263ef99312241b32bd5099e21200d
Eibon7 <Emiliopostigo@gmail.com>
2013-04-17 17:24:36 +0200

    terminada pagina preliminar para revision

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <?php include_once("/calculos/index.html"); ?>
 <?php include_once("/memoria/procesos_recuperacion/index.html"); ?>
 <?php include_once("/preseleccion/index.html"); ?>
+<?php include_once("/psicotecnicas/index.html"); ?>
